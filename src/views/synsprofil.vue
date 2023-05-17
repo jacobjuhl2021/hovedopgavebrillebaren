86dc4fd68d5af5219553dd5f3d72104618883aad
Nikolaihoej <nhje37914@edu.ucl.dk>
2023-05-17 13:55:14 +0200

    sae
    iframe sysnprofil done

--- src/views/synsprofil.vue
+++ src/views/synsprofil.vue
@@ -1,5 +1,12 @@
 <template>
     <backbutton/>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <iframe class="synsprofil" src="https://myvisionprofile.zeiss.com/ecp/Nf0IflUV?fbclid=IwAR3j8WAJ3ZGBgkI_hG9Wiee9hd0R_7OLVj6LgrNHW4HJtKW3Fuh6Y8uHo0Y#intro/introduction-02" frameborder="0"></iframe>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -17,4 +24,9 @@ export default {
 </script>
 
 <style>
+.synsprofil{
+    width: 100%;
+    min-height: 810px;
+}
+
 </style>
