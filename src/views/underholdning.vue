35cf10c128b6dfd0195c59530148ce02b8cb7cc4
Nikolaihoej <nhje37914@edu.ucl.dk>
2023-05-17 10:12:31 +0200

    weq
    sdfs<

--- src/views/underholdning.vue
+++ src/views/underholdning.vue
@@ -1,3 +1,26 @@
 <template>
     <RouterLink to="/home"><button>Gå tilbage</button></RouterLink>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="eyesight_box">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
+
+<style>
+.eyesight_box{
+    position: absolute;
+    width: 654px;
+    height: 467px;
+    left: 76px;
+    top: 124px;
+    background-color: black;
+}
+
+</style>
