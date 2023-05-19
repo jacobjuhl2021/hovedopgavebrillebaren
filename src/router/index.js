d8c3427194278b97bc15df4558f88c2e6981ef50
jacobjuhl2021 <jacobjuhlprivat@gmail.com>
2023-05-19 11:48:01 +0200

    vi er live!!!

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory('/hovedopgave/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/underholdning',
