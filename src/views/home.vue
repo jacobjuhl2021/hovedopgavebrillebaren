e4dfe99355645e360905f934dfc13f2e24bad0c4
Nikolaihoej <nhje37914@edu.ucl.dk>
2023-05-22 09:41:13 +0200

    asd
    asf

--- src/views/home.vue
+++ src/views/home.vue
@@ -10,7 +10,7 @@
                         <div>
                             <RouterLink to="/underholdning"><nav_button class="Nav_position" buttontext = "Underholdning"/></RouterLink>
                         </div>
-                        <img src="../assets/Billede 3 - 70p syn-min.jpg" alt="">
+                        <img src="../assets/Billede 3 - 70p syn-min.jpg" alt="testfucker">
                             <!-- <plantanimation/> -->
                         <div>
                             <RouterLink to="/information"><nav_button class="Nav_position" buttontext = "Information"/></RouterLink>
